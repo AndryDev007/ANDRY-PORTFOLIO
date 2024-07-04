0a079861aae9bb215667515057c068c5de489819
AndryDev007 <andryfanomezana77@gmail.com>
2024-07-04 15:52:36 +0400

    Fix download CV issue on mobile

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -3,21 +3,25 @@ import { Button } from 'react-bootstrap';
 import { FaDownload } from 'react-icons/fa';
 
 const DownloadButton = ({ fileUrl, fileName }) => {
-    const handleDownload = () => {
-      const link = document.createElement('a');
-      link.href = fileUrl;
-      link.download = fileName;
-      document.body.appendChild(link);
-      link.click();
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = fileName;
+    link.target = '_blank';
+    document.body.appendChild(link);
+    link.click();
+
+    setTimeout(() => {
       document.body.removeChild(link);
-    };
-  
-    return (
-      <Button variant="info" onClick={handleDownload}>
-        Download my CV<FaDownload />
-      </Button>
-    );
+      window.URL.revokeObjectURL(link.href);
+    }, 100);
   };
-  
-  export default DownloadButton;
-  
+
+  return (
+    <Button variant="info" onClick={handleDownload}>
+      Download my CV <FaDownload />
+    </Button>
+  );
+};
+
+export default DownloadButton;
